e1c1954cba908ca5d7b8949db0e97dac8f696771
jparmar5268-ship-it <jparmar5268@gmail.com>
2026-01-19 20:40:26 +0530

    operator

---
+++ opt-con/script.js
@@ -0,0 +1,51 @@
+// single line comment
+/*multi-line comment*/
+
+// operators
+
+//  ***Arithmetic Op.***
+
+/*let a = 5;
+let b = 2;
+console.log("a = ", a, " &  b = ", b);
+
+
+// console.log("Addition : ", a + b);
+// console.log("subtraction : ", a - b);
+// console.log("multiplication : ", a * b);
+// console.log("division : ", a / b);
+// console.log("modulus : ", a % b);
+// console.log("exponentiation : ", a ** b);
+
+//Unary Operator
+
+// a = a + 1;
+a++     //post inc.
+a--     //post dec.
+++a;    //pre inc.
+--a;    //pre dec.
+
+console.log("a = ", a);
+console.log("a++ = ", a++);
+console.log("a-- = ", a--);
+console.log("++a = ", ++a);
+console.log("--a = ", --a);     */
+
+
+//  ***Assignment op.***
+
+/*  let a = 5;
+let b = 2;
+
+//a += 4;     a=a+4
+// a -= 4;     a=a-4
+// a /= 4;     a=a/4
+// a *= 4;     a=a*4
+// a %= 4;     a=a%4
+// a **= 4;     a=a**4
+
+console.log("a = ", a);   */
+
+
+//  ***comparison Op.***
+
