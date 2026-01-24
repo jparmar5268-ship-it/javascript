bb6b35f92dd70c1422442660bcdd9065161080ec
jparmar5268-ship-it <jparmar5268@gmail.com>
2026-01-24 18:49:00 +0530

    loop practice complete

--- loop/practice.js
+++ loop/practice.js
@@ -3,5 +3,18 @@
 //print even no. 1 to 100
 
 for (let i = 0; i <= 100; i++){
-    console.log("num = ", num);
+    if (i % 2 == 0) {
+        console.log("i = ", i);
+    }
 }
+
+//Q-2
+
+let gameno = 25;
+
+let usrno = prompt("guess the game number");
+
+while (usrno != gameno) {
+    usrno=prompt("you entered wrong number,guess the game number again");
+}
+console.log("crgs,you entered the right number");
