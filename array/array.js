cdc4132c2441709811b9fec94189b39dff438cd6
jparmar5268-ship-it <jparmar5268@gmail.com>
2026-01-26 16:53:19 +0530

    array and it's practice complete

--- array/array.js
+++ array/array.js
@@ -19,3 +19,57 @@ for (let i = 0; i < heroes.length; i++){
 for (let hero of heroes) {
     console.log(hero);
 }
+
+
+
+//***************Array Method**************//
+
+
+let fooditm = ["apple", "litchi", "patato", "tamato"];
+
+fooditm.push("chips", "burgur", "paneer");  //push()
+console.log(fooditm);
+
+let dlt=fooditm.pop();  //pop()
+console.log(fooditm);
+console.log("Deleted item", dlt);
+
+console.log(fooditm.toString());    //tostring()
+
+let marvel_heros = ["thor", "spiderman", "ironman"];
+let dc_heros = ["superman", "batman"];
+
+let heros = marvel_heros.concat(dc_heros);  //concat()
+console.log(heros);
+
+marvel_heros.unshift("antman"); //unshift() work like push()
+console.log(marvel_heros);
+
+let result = marvel_heros.shift();  //shift() work like pop()
+console.log(result);
+
+//let heroes = ["ironman", "thor", "hulk", "shaktiman", "piderman"];
+
+console.log(heroes);
+
+console.log(heroes.slice(1, 3));    //slice()
+
+let num = [1, 2, 3, 4, 5, 6, 7];
+
+//num.splice(2, 2, 101, 102);   //splice()
+console.log(num);
+
+//add element
+
+//num.splice(2, 0, 101);
+//console.log(num);
+
+//Delet element
+
+//num.splice(3, 1);
+//console.log(num);
+
+//Replace Element
+
+num.splice(3, 1, 101);
+console.log(num);
--- array/practice.js
+++ array/practice.js
@@ -17,10 +17,26 @@ console.log("avg of student is : ", avg);
 let prc = [250, 645, 300, 900, 50];
 let index = 0;
 
-for (let val of prc) {
-    console.log(`value at insex ${index}=${val}`);
-    let ofr = val / 10;
-    prc[index] = prc[index] - ofr;
-    console.log(`value after offer ${prc[index]}`);
-    index++;
+// for (let val of prc) {
+//     console.log(`value at index ${index}=${val}`);
+//     let ofr = val / 10;
+//     prc[index] = prc[index] - ofr;
+//     console.log(`value after offer ${prc[index]}`);
+//     index++;
+// }
+
+for (let i = 0; i < prc.length; i++){
+    let ofr = prc[i] / 10;
+    prc[i] -= ofr;
 }
+
+console.log(prc);
+
+
+//Q-3
+
+
+let companies = ["bloomberg", "microsoft", "uber", "google", "ibm", "netflix"];
+companies.shift();//remove the first com. name
+companies.splice(2, 1, "ola");//replace uber to ola
+companies.push("amazon");//add amazon at last 
